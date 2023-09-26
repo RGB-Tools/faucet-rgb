e378fe2929e1d3cf4c73b0eafb6030a278a53c2e
Nicola Busanello <nicola.busanello@gmail.com>
2023-09-26 09:41:46 +0200

    check requests using xpubs are denied

--- tests/test_basic_operations.py
+++ tests/test_basic_operations.py
@@ -3,7 +3,7 @@ import rgb_lib
 
 from faucet_rgb.utils.wallet import get_sha256_hex
 from tests import fixture_get_app  # pylint:disable=unused-import
-from tests.utils import (OPERATOR_HEADERS, check_receive_asset,
+from tests.utils import (OPERATOR_HEADERS, USER_HEADERS, check_receive_asset,
                          check_requests_left, prepare_user_wallets)
 
 
@@ -30,3 +30,18 @@ def test_receive_asset(get_app):
     app = get_app()
     user = prepare_user_wallets(app, 1)[0]
     check_receive_asset(app, user, None, 200, None)
+
+    # check requests with xPub as wallet ID are denied
+    wallet_id = user["xpub"]
+    wallet = user["wallet"]
+    _ = wallet.create_utxos(user["online"], True, 1, None,
+                            app.config["FEE_RATE"])
+    blind_data = wallet.blind_receive(None, None, None,
+                                      app.config["CONSIGNMENT_ENDPOINTS"], 1)
+    group_query = ""
+    client = app.test_client()
+    resp = client.get(
+        f"/receive/asset/{wallet_id}/{blind_data.recipient_id}{group_query}",
+        headers=USER_HEADERS,
+    )
+    assert resp.status_code == 403
